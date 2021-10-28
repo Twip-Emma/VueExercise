2ae0e74a84ed6e5e635a32ba4d85a5caf86f8a86
七画一只妖 <1157529280@qq.com>
2021-10-28 22:56:57 +0800

    todolist实例完成创建新事务以及nanoid库体验

--- src/App.vue
+++ src/App.vue
@@ -2,39 +2,87 @@
  * @Author: 七画一只妖
  * @Date: 2021-10-13 19:38:00
  * @LastEditors: 七画一只妖
- * @LastEditTime: 2021-10-26 22:44:38
+ * @LastEditTime: 2021-10-28 22:47:23
  * @Description: file content
 -->
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <hr>
-    <HelloWorld1></HelloWorld1>
-    <hr>
-    <HelloWorld2></HelloWorld2>
+  <div id="root">
+    <div class="todo-container">
+      <div class="todo-wrap">
+        <MyHeader></MyHeader>
+        <MyList :todos="todos"></MyList>
+        <MyFooter></MyFooter>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld1 from "./components/Test1.vue"
-import HelloWorld2 from "./components/Test2.vue"
+import MyList from "./components/MyList.vue";
+import MyFooter from "./components/MyFooter.vue";
+import MyHeader from "./components/MyHeader.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld1,
-    HelloWorld2
-  }
-}
+    MyList,
+    MyFooter,
+    MyHeader,
+  },
+  data() {
+    return {
+      todos: [
+        { id: "001", title: "吃饭", done: false },
+        { id: "002", title: "睡觉", done: true },
+        { id: "003", title: "打豆豆", done: false },
+      ],
+    };
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+/*base*/
+body {
+  background: #fff;
+}
+
+.btn {
+  display: inline-block;
+  padding: 4px 12px;
+  margin-bottom: 0;
+  font-size: 14px;
+  line-height: 20px;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  vertical-align: middle;
+  cursor: pointer;
+  box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2),
+    0 1px 2px rgba(0, 0, 0, 0.05);
+  border-radius: 4px;
+}
+
+.btn-danger {
+  color: #fff;
+  background-color: #da4f49;
+  border: 1px solid #bd362f;
+}
+
+.btn-danger:hover {
+  color: #fff;
+  background-color: #bd362f;
+}
+
+.btn:focus {
+  outline: none;
+}
+
+.todo-container {
+  width: 600px;
+  margin: 0 auto;
+}
+.todo-container .todo-wrap {
+  padding: 10px;
+  border: 1px solid #ddd;
+  border-radius: 5px;
 }
 </style>
