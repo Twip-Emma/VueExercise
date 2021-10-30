98edca15e8d1f82f1a49400e01070a585c6f1777
七画一只妖 <1157529280@qq.com>
2021-10-30 21:23:32 +0800

    已完成，todolist案例，第一版

--- src/components/MyFooter.vue
+++ src/components/MyFooter.vue
@@ -2,29 +2,64 @@
  * @Author: 七画一只妖
  * @Date: 2021-10-28 21:45:12
  * @LastEditors: 七画一只妖
- * @LastEditTime: 2021-10-28 22:17:33
+ * @LastEditTime: 2021-10-30 21:13:16
  * @Description: file content
 -->
 <template>
-  <div class="todo-footer">
+  <div class="todo-footer" v-show="allTodoTotal">
     <label>
-      <input type="checkbox" />
+      <input type="checkbox" :checked="isAll" @click="chackAllBtn"/>
     </label>
-    <span> <span>已完成0</span> / 全部2 </span>
-    <button class="btn btn-danger">清除已完成任务</button>
+    <span>
+      <span>已完成{{ doneTodoTotal }}</span> / 全部{{ allTodoTotal }}
+    </span>
+    <button class="btn btn-danger" @click="clearOkTodo">清除已完成任务</button>
   </div>
 </template>
 
 <script>
 export default {
   name: "MyFooter",
+  props: ["todos","checkAll","clearOkTodoReal"],
   data() {
     return {};
+  },
+  computed: {
+    doneTodoTotal() {
+      let i = 0;
+      this.todos.forEach((todo) => {
+        if (todo.done) {
+          i++;
+        }
+      });
+      return i;
+    },
+    allTodoTotal() {
+      return this.todos.length;
+    },
+    isAll() {
+      if (
+        this.doneTodoTotal === this.allTodoTotal &&
+        this.allTodoTotal > 0
+      ) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+  },
+  methods: {
+    chackAllBtn(e){
+      this.checkAll(e.target.checked)
+    },
+    clearOkTodo(){
+      this.clearOkTodoReal()
+    }
   },
 };
 </script>
 
-<style>
+<style scoped>
 /*footer*/
 .todo-footer {
   height: 40px;
