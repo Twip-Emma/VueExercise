5a70cda817077bf539c6c99b6f2a25ee6b170998
七画一只妖 <1157529280@qq.com>
2021-11-07 14:57:38 +0800

    默认插槽

--- src/App.vue
+++ src/App.vue
@@ -2,35 +2,52 @@
  * @Author: 七画一只妖
  * @Date: 2021-10-13 19:38:00
  * @LastEditors: 七画一只妖
- * @LastEditTime: 2021-11-04 19:36:25
+ * @LastEditTime: 2021-11-07 14:57:23
  * @Description: file content
 -->
 <template>
-  <div id="app">
-    <div class="container">
-      <Search />
-      <List />
-    </div>
+  <div class="container">
+    <Category :listData="games" title="游戏">
+      <img
+        src="https://api.pixiv.moe/image/i.pximg.net/img-master/img/2021/11/05/03/07/40/93924717_p0_master1200.jpg"
+        alt=""
+      />
+    </Category>
+    <Category :listData="games" title="电影">
+      <ul>
+        <li v-for="(item, index) in films" :key="index">{{ item }}</li>
+      </ul>
+    </Category>
+    <Category :listData="games" title="美食">
+      <img
+        src="https://api.pixiv.moe/image/i.pximg.net/img-master/img/2021/11/05/00/30/00/93922241_p0_master1200.jpg"
+        alt=""
+      />
+    </Category>
   </div>
 </template>
 
 <script>
-import List from "./components/List"
-import Search from "./components/Search"
+import Category from "./components/Category";
+// import Search from "./components/Search"
 // import axios from 'axios'
 
 export default {
-  name:"App",
-  components:{List,Search},
+  name: "App",
+  components: { Category },
   data() {
     return {
-      
-    }
-  }
+      games: ["英雄联盟", "穿越火线", "DOTA"],
+      films: ["《你好，李焕英》", "《悟空传》", "《唐人街探案3》"],
+      foods: ["饼干", "蛋糕", "鸡腿"],
+    };
+  },
 };
 </script>
 
 <style>
-
-
+.container {
+  display: flex;
+  justify-content: space-around;
+}
 </style>
